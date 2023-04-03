3630665ba43b5172a2b6945ca72383e958319f41
v-kirilov <v_kirilov87@protonmail.ch>
2023-04-03 21:48:39 +0300

    Html Scraper Initial

---
+++ HTML_Scraper/HTML_Scraper/Program.cs
@@ -0,0 +1,67 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+//Set up AngleSharp
+var config = Configuration.Default.WithDefaultLoader();
+var context = BrowsingContext.New(config);
+
+string url = string.Empty;
+
+//Parallel for doing async function in different proccess!
+Parallel.For(1, 10, (i) =>
+{
+    Console.WriteLine(i);
+    //await GetRecipe(context,i);
+});
+
+while (true)
+
+
+{
+    Console.WriteLine("Enter city:");
+    Console.WriteLine("s-Sofia ; p-Plovdiv ; k-Kyustendil");
+    url = Console.ReadLine();
+
+    if (url == "s")
+    {
+        url = "https://www.sinoptik.bg/sofia-bulgaria-100727011/10-days";
+        break;
+    }
+    else if (url == "p")
+    {
+        url = "https://www.sinoptik.bg/plovdiv-bulgaria-100728193/10-days";
+        break;
+    }
+    else if (url == "k")
+    {
+        url = "https://www.sinoptik.bg/kyustendil-bulgaria-100729730/10-days";
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Input valid city!");
+    }
+}
+
+
+
+//Take url
+await GetWeather(context, url);
+
+static async Task GetWeather(IBrowsingContext context, string url)
+{
+    var document = await context.OpenAsync(url);
+    var recipeNameAndCategory = document
+        .QuerySelectorAll("a.wf10dayRight");
+
+    foreach (var item in recipeNameAndCategory)
+    {
+        var data = item.TextContent.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+        Console.WriteLine($"Date: {data[1]}");
+        Console.WriteLine($"Max temp: {data[3]}");
+        Console.WriteLine($"Min temp: {data[5]}");
+        Console.WriteLine($"Wind speed: {data[8]}");
+        Console.WriteLine();
+
+    }
+}
---
+++ HTML_Scraper/HTML_Scraper/README.md
@@ -0,0 +1,2 @@
+A console app. made using AngleSharp to scrape so data from Sinoptik.bg for 10 days weather report.
+You can choose city Sofia/Plovdiv/Kyustenil
