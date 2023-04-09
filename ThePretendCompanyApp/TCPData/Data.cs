016a964be154df1658703fd74ce6aadfbb544f71
v-kirilov <v_kirilov87@protonmail.ch>
2023-04-09 22:09:24 +0300

    Advanced C# – LINQ Initial commit.

---
+++ ThePretendCompanyApp/TCPData/Data.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPData
+{
+    public static class Data
+    {
+       public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee employee = new()
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Jones",
+                AnnualSalary = 60000.1m,
+                IsManager = true,
+                Department = 1
+            };
+            employees.Add(employee);
+            employee = new()
+            {
+                Id = 2,
+                FirstName = "Sarah",
+                LastName = "Jameson",
+                AnnualSalary = 80000.1m,
+                IsManager = true,
+                Department = 2
+            };
+            employees.Add(employee);
+
+            employee = new()
+            {
+                Id = 3,
+                FirstName = "Douglas",
+                LastName = "Roberts",
+                AnnualSalary = 40000.2m,
+                IsManager = false,
+                Department = 2
+            };
+            employees.Add(employee);
+
+            employee = new()
+            {
+                Id = 3,
+                FirstName = "Jane",
+                LastName = "Stevens",
+                AnnualSalary = 30000.3m,
+                IsManager = false,
+                Department = 3
+            };
+            employees.Add(employee);
+
+            return employees;
+        }
+
+        public static List<Department> GetDepartments()
+        {
+            List<Department> departments = new();
+
+            Department department = new()
+            {
+                Id = 1,
+                ShortName = "HR",
+                LongName = "Human Resourses"
+            };
+            departments.Add(department);
+
+            department = new()
+            {
+                Id = 2,
+                ShortName = "FN",
+                LongName = "Finance"
+            };
+            departments.Add(department);
+
+            department = new()
+            {
+                Id = 3,
+                ShortName = "TE",
+                LongName = "Technology"
+            };
+            departments.Add(department);
+
+            return departments;
+        }
+    }
+    
+}
---
+++ ThePretendCompanyApp/ThePretendCompanyApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using TCPData;
+using TCPExtensions;
+
+List<Employee> employeeList = Data.GetEmployees();
+
+var filteredEmployees = employeeList.Filter(x => x.IsManager == true);
+
+foreach (var employee in filteredEmployees)
+{
+    Console.WriteLine($"First name: {employee.FirstName}");
+    Console.WriteLine($"Last name: {employee.LastName}");
+    Console.WriteLine($"Annual salary: {employee.AnnualSalary}");
+    Console.WriteLine($"Manager: {employee.IsManager}");
+    Console.WriteLine();
+}
+
+List<Department> departmentsList = Data.GetDepartments();
+
+var filteredDepartments = departmentsList.Filter(x => x.ShortName == "HR" || x.ShortName == "FN");
+
+foreach (var dep in filteredDepartments)
+{
+    Console.WriteLine($"Id: {dep.Id}");
+    Console.WriteLine($"Short name: {dep.ShortName}");
+    Console.WriteLine($"Long name: {dep.LongName}");
+
+}
+Console.ReadKey();
